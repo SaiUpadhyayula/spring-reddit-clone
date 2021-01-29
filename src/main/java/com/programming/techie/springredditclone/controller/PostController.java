0ef0cd7323938a3985d57aa302dcd27b28d43c50
NIKHIL WANI <65514201+Nikhil-Wani@users.noreply.github.com>
2021-01-30 01:43:34 +0530

    @PathVariable
    @PathVariable String name

--- src/main/java/com/programming/techie/springredditclone/controller/PostController.java
+++ src/main/java/com/programming/techie/springredditclone/controller/PostController.java
@@ -41,7 +41,7 @@ public class PostController {
     }
 
     @GetMapping("by-user/{name}")
-    public ResponseEntity<List<PostResponse>> getPostsByUsername(String username) {
-        return status(HttpStatus.OK).body(postService.getPostsByUsername(username));
+    public ResponseEntity<List<PostResponse>> getPostsByUsername(@PathVariable String name) {
+        return status(HttpStatus.OK).body(postService.getPostsByUsername(name));
     }
 }
